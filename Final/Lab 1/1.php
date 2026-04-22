f7a7ca7394a9d82f3ca0392a90a08e76c8accabc
Osama-Mobin <zuharbhai535@gmail.com>
2026-04-22 17:33:44 +0600

    Lab 1 and 2 commited

---
+++ Final/Lab 1/1.php
@@ -0,0 +1,22 @@
+
+<html>
+    <head>
+        <title>Lab 1 - Task 1</title>
+    </head>
+    <body>
+        <h1>Task 1: Calculate Area and Perimeter of a Rectangle</h1>
+    </body>
+</html>
+
+
+<?php
+    $l = 10;
+    $w = 5;
+    $a = $l * $w;
+    $p = 2 * ($l + $w);
+
+    echo "Length = {$l}<br>";
+    echo "Width = {$w}<br>";
+    echo "Area of Rectangle = {$a}<br>";
+    echo "Perimeter of Rectangle = {$p}<br>";
+?>
